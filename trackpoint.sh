8f92ff83b23447c6fdd05299e623e95b83eaf386
nesro <nesro@nesro.cz>
2014-09-16 22:40:08 +0200

    trackpoint

---
+++ trackpoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if ! id | grep root; then
+	echo "you need to be root"
+	exit 1
+fi
+
+synclient TouchpadOff=1
+echo -n 255 > /sys/devices/platform/i8042/serio1/serio2/speed
+echo -n 255 > /sys/devices/platform/i8042/serio1/serio2/sensitivity
+
